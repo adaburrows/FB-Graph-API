e987d4bb0b1a2b039f1057a5479ca2530bf79a3f
Jillian Ada Burrows <Jillian@RepresentationDesign.net>
2010-05-11 13:25:47 -0700

    Initial Commit

---
+++ cake/components/fb.php
@@ -0,0 +1,215 @@
+<?php
+/* FbComponent
+ * An easy to use, basic implementation of Facebook's Graph API for CakePHP
+ *==============================================================================
+ * -- Version alpha 0.1 --
+ * The source code is fairly well documented, except for the base class it
+ * derives from.
+ * This code is being released under an MIT style license:
+ *
+ * Copyright (c) 2010 Jillian Ada Burrows
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *------------------------------------------------------------------------------
+ * Original Author:		Jillian Ada Burrows
+ * Email:			jill@adaburrows.com
+ * Website:			<http://www.adaburrows.com>
+ * Facebook:			<http://www.facebook.com/jillian.burrows>
+ * Twitter:			@jburrows
+ *------------------------------------------------------------------------------
+ * Use at your own peril! J/K
+ * 
+ * Here's an example use in a controller:
+ * <?php 
+ * class FbTestController extends AppController {
+ * 	var $name = 'FbTest';
+ * 	var $pageTitle = 'The Test of FbComponent';
+ * 	var $components = array('Fb');
+ * 
+ * 	function beforeFilter() {
+ * 		parent::beforeFilter();
+ * 		$this->Auth->allowedActions = array('index', 'fb_index');
+ * 		$this->Fb->set_redirect_uri('http://adaburrows.com/fb_test/');
+ * 	}
+ * 
+ * 	function index() {
+ * 		$this->Fb->auth_redirect(array('publish_stream','create_event','rsvp_event'));
+ * 		$data = null;
+ * 		if($this->Fb->access_token()) {
+ * 			$data = $this->Fb->request_object('me');
+ * 		}
+ * 		$this->set('response', $data);
+ * 	}
+ * }
+ */
+
+App::import('Component', 'HttpRequest');
+class FbComponent extends HttpRequestComponent {
+	var $name="Fb";
+
+	//Facebook API domain
+	var $fb_d = 'graph.facebook.com';
+	//Facebook oAuth endpoint
+	var $fb_oauth = '/oauth/';
+	//Facebook variables to initalize later
+	var $fb_id;
+	var $fb_key;
+	var $fb_secret;
+
+	//redirect URI (on your server)
+	var $redirect_uri;
+	//store the token here
+	var $token;
+	//store the current controller reference here
+	var $controller;
+
+	function initialize(&$controller){
+		//Store a reference to the current controller for redirects, etc/
+		$this->controller = &$controller;
+		//Load Facebook application value from the configuration
+		$this->fb_id		= Configure::read('App.fb_id');
+		$this->fb_key		= Configure::read('App.fb_key');
+		$this->fb_secret	= Configure::read('App.fb_secret');
+		//set up the specifics for connecting to facebook's api
+		$this->request_params['port']		= 443;
+		$this->request_params['scheme']		= 'tls://';
+		$this->request_params['host']		= $this->fb_d;
+	}
+
+	/* set_redirect_uri
+	 * ----------------
+	 * Sets the URI that face will redirect the user to after authentication
+	 */
+	function set_redirect_uri($uri) {
+		$this->redirect_uri = $uri;
+	}
+
+	/* auth_redirect
+	 * -------------
+	 * Redirects the user to the autication page if necessary.
+	 * You can set the permissions you would like by passing in an array of permissions:
+	 *     auth_redirect( array( 'publish_stream', 'create_event', 'rsvp_event' ) );
+	 * See <http://developers.facebook.com/docs/authentication/permissions>
+	 * for a full list of permissions.
+	 */
+	function auth_redirect($permissions=null) {
+		if (!isset($_GET['code'])) {
+			$redirect_url  = "https://{$this->fb_d}{$this->fb_oauth}authorize?";
+			$redirect_url .= "client_id={$this->fb_id}";
+			$redirect_url .= "&redirect_uri={$this->redirect_uri}";
+			$redirect_url .= $permissions!=null ? '&'.implode(',', $permissions) : '';
+			$this->controller->redirect($redirect_url);
+
+		}
+	}
+
+	/* get_token
+	 * ---------
+	 * After authentication is requested this function will return true if 
+	 * the access token was retreived successfully. The access token is stored
+	 * in $this->token.
+	 */
+	function get_token() {
+		$this->request_params['path']		= $this->fb_oauth.'access_token';
+		$this->request_params['query_params']	= array(
+				'client_id'	=> $this->fb_id,
+				'redirect_uri'	=> $this->redirect_uri,
+				'client_secret'	=> $this->fb_secret,
+				'code'		=> $_GET['code']
+			);
+
+		$data = $this->do_request() ? $this->explode_query($this->get_data()) : null;
+		$access_token = isset($data['access_token']) ? $data['access_token'] : null;
+		$this->token = $access_token;
+		return $access_token!=null;
+	}
+
+	/* get_object
+	 * ----------
+	 * Requests an object's basic info from the FB social graph. Returns data
+	 * as a hash with keys that match the expected values as specified at:
+	 * <http://developers.facebook.com/docs/api>
+	 */
+	function get_object($object) {
+		$this->request_params['path']		= $object;
+		$this->request_params['query_params']	= array(
+				'access_token' => $this->token
+			);
+		$object = $this->do_request() ? $this->get_data() : null;
+		if ($object != null) {
+			$object = json_decode($object, true);
+		}
+		return $object;	
+	}
+
+	/* get_connection_types
+	 * --------------------
+	 * Requests an object's connection types to the FB social graph. Returns
+	 * a hash of connection types as keys and links to the respective api
+	 * calls.
+	 * <http://developers.facebook.com/docs/api>
+	 */
+	function get_connection_types($object) {
+		$this->request_params['path']		= $object;
+		$this->request_params['query_params']	= array(
+				'metadata'	=> 1,
+				'access_token'	=> $this->token
+			);
+		$object = $this->do_request() ? $this->get_data() : null;
+		if ($object != null) {
+			$object = json_decode($object, true);
+		}
+		return $object['metadata']['connections'];	
+	}
+
+	/* get_connections
+	 * ---------------
+	 * Requests an object's connections to other objects on the FB graph.
+	 * $connection_type can be: any of: (friends, home, feed (Wall), likes, 
+	 * movies, books, notes, photos, videos, events, groups).
+	 * However, call get_connection_types to get a real list of connection
+	 * an object supports.
+	 * <http://developers.facebook.com/docs/api>
+	 */
+	function get_connections($object, $connection_type) {
+		$this->request_params['path']		= "$object/$connection_type";
+		$this->request_params['query_params']	= array(
+				'access_token' => $this->token
+			);
+		$object = $this->do_request() ? $this->get_data() : null;
+		if ($object != null) {
+			$object = json_decode($object, true);
+		}
+		return isset($object['data']) ? $object['data'] : null;
+	}
+
+	function post_feed($object, $message_params) {
+		$this->request_params['method']		= 'POST';
+		$this->request_params['path']		= "$object/feed";
+		$this->request_params['query_params']	= array_merge(array(
+				'access_token' => $this->token
+			), $message_params);
+		$this->request_params['body']		= $this->build_query($message_params);
+		$object = $this->do_request() ? $this->get_data() : null;
+		if ($object != null) {
+			$object = json_decode($object, true);
+		}
+		return $object;
+	}
+}
---
+++ cake/components/http_request.php
@@ -0,0 +1,207 @@
+<?php
+/* HttpRequestComponent:
+ * A basic implementation of an Http Client (currently in the style of CakePHP)
+ *==============================================================================
+ * -- Version alpha 0.1 --
+ * This code is being released under an MIT style license:
+ *
+ * Copyright (c) 2010 Jillian Ada Burrows
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *------------------------------------------------------------------------------
+ * Original Author:		Jillian Ada Burrows
+ * Email:			jill@adaburrows.com
+ * Website:			<http://www.adaburrows.com>
+ * Facebook:			<http://www.facebook.com/jillian.burrows>
+ * Twitter:			@jburrows
+ *------------------------------------------------------------------------------
+ * Use at your own peril! J/K
+ * 
+ * This was meant to be used with the FbComponent class, see it for usage hints.
+ */
+
+class HttpRequestComponent extends Object {
+	var $name="HttpRequest";
+
+	//basic request parameters
+	var $request_params = array(
+			'port'		=> 80,
+			'scheme'	=> '',
+			'host'		=> 'localhost',
+			'path'		=> '/',
+			'method'	=> 'GET'
+	);
+	//string represntation of the request
+	var $request = '';
+	//array representation of the response
+	var $response = array();
+
+	/* explode_query
+	 * -------------
+	 * Takes a query string and turns it into a hash
+	 */
+	function explode_query($q) {
+		$q = explode("&", $q);
+		foreach($q as $value) {
+			$datum = explode("=", $value);
+			$k = rawurldecode(array_shift($datum));
+			$v = rawurldecode(array_shift($datum));
+			$data[$k] = $v;
+		}
+		return $data;
+	}
+
+	/* build_query
+	 * -----------
+	 * Builds a query string from a query hash.
+	 */
+	function build_query() {
+		foreach($this->request_params['query_params'] as $key => $value) {
+			$k = rawurlencode($key);
+			$v = rawurlencode($value);
+			$q[] = "$k=$v";
+		}
+		$query = implode("&", $q);
+		return $query;
+	}
+
+	/* explode_headers
+	 * ---------------
+	 * Takes an array of string header lines and turns it into a hash format.
+	 */
+	function explode_headers($h) {
+		$data = array();
+		foreach($h as $header) {
+			$datum = explode(": ", $header);
+			$k = rawurldecode(array_shift($datum));
+			$v = rawurldecode(array_shift($datum));
+			$data[$k] = $v;
+		}
+		return $data;
+	}
+
+	/* build_headers
+	 * -------------
+	 * Takes an header array and returns a string version.
+	 */
+	function build_headers() {
+		foreach($this->request_params['header_params'] as $key => $value) {
+			$header_line[] = "$key: $value";
+		}
+		$headers = implode("\r\n", $header_line);
+		return $headers;
+	}
+
+	/* build_request
+	 * -------------
+	 * Takes the request array and turns it into a usable string.
+	 */
+	function build_request() {
+		$query = (isset($this->request_params['query_params'])) ? ('?'.$this->build_query()) : '';
+		$request  = "{$this->request_params['method']} {$this->request_params['path']}$query HTTP/1.1\r\n";
+		$request .= "Host: {$this->request_params['host']}\r\n";
+		$request .= "Connection: Close\r\n";
+		if ($this->request_params['method'] == 'POST') {
+			$request .= 'Content-Length: '.strlen($this->request_params['body'])."\r\n";
+			$request .= "Content-Type: application/x-www-form-urlencoded\r\n";
+		}
+		if (isset($this->request_params['header_params'])) {
+			$request .= $this->build_headers();
+		}
+		$request .= "\r\n";
+		if (isset($this->request_params['body'])) {
+			$request .= $this->request_params['body'];
+		}
+		$request .= "\r\n";
+		$this->request = $request;
+	}
+
+	/* tx_request
+	 * ----------
+	 * Transceives the current request. Opens socket and submits request,
+	 * reads response, closes the connection, then return the response.
+	 */
+	function tx_request() {
+		$scheme = isset($this->request_params['scheme']) ? $this->request_params['scheme'] : '';
+		$port = isset($this->request_params['port']) ? $this->request_params['port'] : 80;
+		$fp = @fsockopen($scheme.$this->request_params['host'], $port);
+		//TODO: add better error handling than this!
+		if (!is_resource($fp)){
+			die('connection failed');
+		}
+		$this->build_request();
+		if (!fputs($fp, $this->request, strlen($this->request))) {
+			fclose($fp);
+			die('request failed');
+		}
+		$response = '';
+		while (!feof($fp)){
+			$response .= fread($fp, 4096);
+		}
+		fclose($fp);
+		return($response);
+	}
+
+	/* parse_response
+	 * --------------
+	 * Takes the string representation of the response and parses it into
+	 * it's components for easy digestion.
+	 */
+	function parse_response($response) {
+		$r = explode("\r\n", $response);
+
+		$status = array_shift($r);
+		$status = explode(" ", $status);
+		$protocol = array_shift($status);
+		$protocol = explode('/', $protocol);
+
+		$this->response['status'] = array_shift($status);
+		$this->response['reason'] = array_shift($status);
+		$this->response['protocol'] = array_shift($protocol);
+		$this->response['protocol_version'] = array_shift($protocol);
+		$this->response['body'] = array_pop($r);
+		$this->response['headers'] = $this->explode_headers($r);
+
+		return $this->response['status'];
+	}
+
+	/* do_request
+	 * ----------
+	 * Executes the request and parses status
+	 */
+	function do_request() {
+		$resp = $this->tx_request();
+		$status = $this->parse_response($resp);
+		//TODO: parse response and generate error message
+		$r_stat = true;
+		//Reset method to 'GET' for next call
+		$this->request_params['method'] = 'GET';
+		return $r_stat;
+	}
+
+	/* get_data
+	 * --------
+	 * Fetches the data from the reponse
+	 */
+	function get_data() {
+		//TODO: make this return null when not set
+		return $this->response['body'];
+	}
+
+}
